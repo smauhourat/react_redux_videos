48378ffc5fd95b83905a16f0a82bc8f0b01d77d7
SMauhourat <santiagomauhourat@hotmail.com>
2021-12-19 17:44:31 -0300

    video player

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const VideoDetails = (props) => {
+    const videoUrl = `https://www.youtube.com/embed/${props.video?.id.videoId}`;
     return (
         <div>
-            VideoDetails: {props.video?.snippet.title}
+            <div className="ui embed">
+                <iframe src={videoUrl} />
+            </div>
+            <div className="ui segment">
+                <h4 className="header">{props.video?.snippet.title}</h4>
+                <p>{props.video?.snippet.description}</p>
+            </div>
         </div>
     )
 }
